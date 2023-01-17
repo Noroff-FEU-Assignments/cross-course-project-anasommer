b8a33a57ceaee5744a454e4ea413b3264535c8de
Anastassia Sommer <anastassia.sommer@gmail.com>
2023-01-17 15:30:08 +0100

    integration with WP API added

--- js/womens.js
+++ js/womens.js
@@ -1,22 +1,32 @@
-import jacketsData from "./data.js";
-
 const jacketsContainer = document.querySelector("#jackets-container");
 
-// Render jackets to the page
-jacketsData.map((jacket) => {
-  if (jacket.sex === "female") {
-    jacketsContainer.innerHTML += ` 
-                                    <section class="jacket-item">
-                                    <a href="/item-page.html?id=${jacket.id}"
-                                        ><img src="${jacket.img}" class="jacket-image"
-                                    /></a>
-                                    <h2 class="jacket-h2 futura-font-bold">${jacket.name}</h2>
-                                    <p class="jacket-desc roboto-font">
-                                        ${jacket.shortDesc}
-                                        <span class="jacket-price futura-font-bold">NOK ${jacket.price}.-</span>
-                                    </p>
-                                    <a href="/item-page.html?id=${jacket.id}" class="btn-add roboto-font">Show jacket</a>
-                                    </section>
-                                    `;
-  }
-});
+const baseUrl = `https://anasommer.com/api/wp-json/wc/store/products`;
+
+async function renderProducts(url) {
+  const response = await fetch(url);
+  const jacketsData = await response.json();
+
+  // Render jackets to the page
+  jacketsData.map((jacket) => {
+    if (jacket.categories[0]) {
+      const jacketShortDescription = jacket.short_description.slice(3, -4);
+
+      jacketsContainer.innerHTML += ` 
+      <section class="jacket-item">
+      <a href="/item-page.html?id=${jacket.id}">
+      <img src="${jacket.images[0].src}" class="jacket-image"/>
+      </a>
+      <h2 class="jacket-h2 futura-font-bold">${jacket.name}</h2>
+      <p class="jacket-desc roboto-font">${jacketShortDescription}
+          <span class="jacket-price futura-font-bold">NOK ${jacket.prices.price}.-</span>
+    
+     
+      </p>
+      <a href="/item-page.html?id=${jacket.id}" class="btn-add roboto-font">Show jacket</a>
+      </section>
+    `;
+    }
+  });
+}
+
+renderProducts(baseUrl);
